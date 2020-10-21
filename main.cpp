97e5c80a98061ab7cba06aacbce1e59b34d3b8ab
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-10-21 12:24:31 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,35 @@
+// main.cpp 
+// Степанчук Сергій 
+// Лабораторна робота № 3.3 
+// Розгалуження, задане графіком функції. 
+// Варіант 20
+
+#include <iostream> 
+#include <cmath>
+
+using namespace std;
+
+void main() {
+    double x, y, R; // input
+
+    cout << "R = "; cin >> R;  
+
+    if (R > 3) {
+        cout << "R < 3 must" << endl;
+        return;
+    }
+
+    cout << "x = "; cin >> x;
+
+    if (x < 0)
+        y = x / -2;
+    else if (x == 0)
+        y = 0;
+    else if (x <= R)
+        y = R - sqrt(R * R - x * x);
+    else if (x <= 2 * R)
+        y = sqrt(R * R - pow(x - R, 2));
+    else y = x / -6;
+
+    cout << endl << "y = " << y << endl;
+}
